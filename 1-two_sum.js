5bfae81bd6912f44dfcec031c804cf4118022a0e
Kadir Zeybekoğlu <kadrzeybek@gmail.com>
2025-05-13 00:27:49 +0300

    121-best_time_to_buy_and_sell_stock done

--- 1-two_sum.js
+++ 1-two_sum.js
@@ -27,23 +27,6 @@ var twoSum = function(nums, target){
     }
 }
 
-
-
-
-
-// var twoSum = function(nums, target) {
-//     let map = new Map();
-//     for (let i = 0; i < nums.length; i++) {
-//         let complement = target - nums[i];
-
-//         if (map.has(complement)) {
-//             return [map.get(complement), i];
-//         }
-
-//         map.set(nums[i], i);
-//     }
-
-// };
 //test
 let nums = [4,7,11,15]
 let target = 15
---
+++ 121-best_time_to_buy_and_sell_stock.js
@@ -0,0 +1,37 @@
+//my brute force solution but it is bad for big data
+/*
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+    for (let i = 0; i < prices.length - 1; i++) {
+        for (let j = i + 1; j < prices.length; j++) {
+            let profit = prices[j] - prices[i];
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+    }
+    return maxProfit;
+};
+*/
+
+//optimize solution
+var maxProfit = function(prices) {
+    let minValue = prices[0];
+    let maxPorfit = 0;
+    for(let i=1;i<prices.length;i++){
+        let profit = prices[i] - minValue
+        
+        if(profit>maxPorfit){
+            maxPorfit = profit
+        }
+        if(prices[i]< minValue){
+            minValue = prices[i]
+        }
+    }
+    return maxPorfit
+};
+
+//test
+let prices = [2,5,1,6,2,7,5];
+let result = maxProfit(prices);
+console.log(result);  // 6
